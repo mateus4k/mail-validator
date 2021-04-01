4a23c4fc7a026a88117a249e0d713b6034c39023
Mateus Sampaio <mateus4k@protonmail.ch>
2021-04-01 16:19:08 -0300

    security improvements

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { BullModule } from '@nestjs/bull';
+import { ThrottlerModule } from '@nestjs/throttler';
 import { MailsModule } from './mails/mails.module';
 
 @Module({
   imports: [
+    ThrottlerModule.forRoot({
+      ttl: 60,
+      limit: 10,
+    }),
     MongooseModule.forRoot('mongodb://localhost/nest', {
       useCreateIndex: true,
     }),
